b631fc683e76da429059f09b4f6943357ae19fce
FabianoG <boriscasoy123@gmail.com>
2023-10-24 23:23:14 -0300

    Atualização de cor

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-let btnSeeMore = document.querySelectorAll('.about-text-btn')[0]
+
 let divAbout = document.querySelectorAll('.about')[0]
 let moreText = document.querySelectorAll('.about-text-more')[0]
+let about = document.querySelectorAll('.about')[0]
+let project = document.querySelectorAll('.projects')[0]
 let viewMore = false
-
-console.log(divAbout);
-btnSeeMore.addEventListener('click', ()=>{
-    if (viewMore == false) {
-        divAbout.style.transition = '400ms'
-        // divAbout.style.height = "950px"
-        moreText.style.height = 'auto'
-
-        btnSeeMore.textContent = "Ver menos"
-        viewMore = true
-    } else {
-        moreText.style.height = '0'
-        btnSeeMore.textContent = "Ver mais..."
-        viewMore = false
-    }
-
-})
