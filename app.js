2a826a440b2ce7f61849ae196fa17f39df9fb3bc
FabianoG <boriscasoy123@gmail.com>
2023-10-25 13:34:13 -0300

    atualização no estilo e cor  e iclusao de filtro

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 
-let divAbout = document.querySelectorAll('.about')[0]
-let moreText = document.querySelectorAll('.about-text-more')[0]
-let about = document.querySelectorAll('.about')[0]
-let project = document.querySelectorAll('.projects')[0]
-let viewMore = false
+let filter = document.querySelectorAll('.projects-filter')[0]
+
+let btnFilter = filter.querySelectorAll("span")
+
+let main
+
+let mainProject = document.querySelectorAll('.main')
+let secProject = document.querySelectorAll('.sec')
+
+btnFilter.forEach(element => {
+    element.addEventListener('click', filteProject)
+});
+
+
+
+function filteProject() {
+    let child = this.parentElement.querySelectorAll('span')
+    child.forEach(element => {
+        element.style.background = ""
+        element.style.color = ""
+    });
+    this.style.background = '#333'
+    this.style.color = '#fdfdfd'
+    if (this.textContent == "Principais") {
+        mainShow("block")
+        secShow("none")
+
+    } else if (this.textContent == "Secundários") {
+        mainShow("none")
+        secShow("block")
+    } else { 
+        mainShow("block")
+        secShow("block")
+    }
+}
+
+function mainShow(e) {
+    mainProject.forEach(element => {
+        element.style.display = e
+    });
+}
+function secShow(e) {
+    secProject.forEach(element => {
+        element.style.display = e
+    });
+}
