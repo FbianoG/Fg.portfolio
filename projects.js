6ac2664b7f1b8c6760b3d8755410b98a3131bb96
F.Gonçalves <boriscasoy123@gmail.com>
2024-03-20 18:39:55 -0300

    chore: delete projects.js

--- projects.js
+++
@@ -1,9 +0,0 @@
-let projects = [
-    {
-        name:"FG.Torrent",
-        main: true,
-        webCode: "",
-        webLink: "https://fg-torrent.onrender.com/",
-        src: "img/FG.torrent.png",
-    }
-]
